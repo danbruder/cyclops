f431380600efe440af4634154f5b077c0e46f000
Dan Bruder <danb@structionsite.com>
2021-02-02 21:01:39 -0500

    match authority

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use warp::host::Authority;
 use warp::{http::Response, hyper::body::Bytes, Filter, Rejection, Reply};
-use warp_reverse_proxy::reverse_proxy_filter;
+
+use warp_reverse_proxy::{extract_request_data_filter, proxy_to_and_forward_response};
 
 async fn log_response(response: Response<Bytes>) -> Result<impl Reply, Rejection> {
     println!("{:?}", response);
@@ -8,16 +10,30 @@ async fn log_response(response: Response<Bytes>) -> Result<impl Reply, Rejection
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let hello = |name| warp::path!("hello").map(move || format!("Hello port foo, {}!", name));
 
     // // spawn base server
-    tokio::spawn(warp::serve(hello).run(([0, 0, 0, 0], 8080)));
+    tokio::spawn(warp::serve(hello("8080")).run(([0, 0, 0, 0], 8080)));
+    tokio::spawn(warp::serve(hello("8081")).run(([0, 0, 0, 0], 8081)));
+    tokio::spawn(warp::serve(hello("8082")).run(([0, 0, 0, 0], 8082)));
 
-    // Forward request to localhost in other port
-    let app = warp::path!("hello" / ..).and(
-        reverse_proxy_filter("".to_string(), "http://127.0.0.1:8080/".to_string())
-            .and_then(log_response),
-    );
+    let request_filter = extract_request_data_filter();
+    let app = warp::host::optional()
+        // build proxy address and base path data from current filter
+        .map(|authority: Option<Authority>| {
+            let port = match authority.as_ref().map(|a| a.host()) {
+                Some("foo.danbruder.com") => 8080,
+                Some("bar.danbruder.com") => 8081,
+                Some(_) => 8082,
+                None => 8082,
+            };
+            (format!("http://127.0.0.1:{}/", port), "".to_string())
+        })
+        .untuple_one()
+        // build the request with data from previous filters
+        .and(request_filter)
+        .and_then(proxy_to_and_forward_response)
+        .and_then(log_response);
 
     // spawn proxy server
     warp::serve(app).run(([0, 0, 0, 0], 3030)).await;
